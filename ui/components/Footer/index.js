ae78254a9b4543e8dd599aca601ec070dfbaeb03
Sven Lindstroem <svenlindstroem@Svens-MBP.fritz.box>
2019-09-06 14:31:31 +0200

    removing links

--- ui/components/Footer/index.js
+++ ui/components/Footer/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Grid } from 'react-bootstrap';
 import { Meteor } from 'meteor/meteor';
 import { year } from '../../../modules/dates';
@@ -20,20 +19,6 @@ const Footer = () => (
         &copy;
         {` ${copyrightYear()} ${productName}`}
       </p>
-      <ul className="pull-right">
-        <li>
-          <Link to="/terms">
-            Terms
-            <span className="hidden-xs"> of Service</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="/privacy">
-            Privacy
-            <span className="hidden-xs"> Policy</span>
-          </Link>
-        </li>
-      </ul>
     </Grid>
   </Styles.Footer>
 );
